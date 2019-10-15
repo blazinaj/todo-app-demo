173e26528a1c5b6ddbf7dc119c957a6081d24ddf
Jacob Blazina <blazinaj@hotmail.com>
2019-10-14 20:56:19 -0700

    Updates

---
+++ precache-manifest.d57cece1fb7b8db5b7b4f11afc41d95b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a77a164b9eddf82a63ddaebf6f9337",
+    "url": "/todo-app-demo/index.html"
+  },
+  {
+    "revision": "fbdf0e723a90865ae41a",
+    "url": "/todo-app-demo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "37a8006fc75a484482d5",
+    "url": "/todo-app-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fbdf0e723a90865ae41a",
+    "url": "/todo-app-demo/static/js/2.d5845966.chunk.js"
+  },
+  {
+    "revision": "37a8006fc75a484482d5",
+    "url": "/todo-app-demo/static/js/main.6de7461c.chunk.js"
+  },
+  {
+    "revision": "505de13ae4dd416f9660",
+    "url": "/todo-app-demo/static/js/runtime-main.faa6305a.js"
+  }
+]);
